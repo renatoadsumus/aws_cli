97ac6fc16c2dd1ae10552866057637906db86894
renatoadsumus <renatoadsumus@gmail.com>
2018-05-23 18:44:32 -0300

    Incluindo parametro -j na forma de fazer ZIP com objetivo de nao levar em consideracao o path completo

--- criar_bucket_s3.sh
+++ criar_bucket_s3.sh
@@ -4,7 +4,7 @@ aws s3api create-bucket --bucket bucket-geru-renato-coutinho --region us-east-1 
 sleep 2
 git clone https://github.com/renatoadsumus/geru_app.git artefato_deploy
 sleep 2
-zip -r app_geru_eb.zip /artefato_deploy/aws_eb/
+zip -r -j app_geru_eb.zip /artefato_deploy/aws_eb/
 echo "Enviando artefato_deploy para S3"
 sleep 2
 aws s3 cp app_geru_eb.zip s3://bucket-geru-renato-coutinho
