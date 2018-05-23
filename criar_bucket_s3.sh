f855b59f24ac67c5273b0f6d1287578d080a86c4
renatoadsumus <renatoadsumus@gmail.com>
2018-05-23 16:41:38 -0300

    Alterando caminho para realizar o zip do servico EB

--- criar_bucket_s3.sh
+++ criar_bucket_s3.sh
@@ -2,6 +2,6 @@
 echo "Criando Bucket S3"
 aws s3api create-bucket --bucket bucket_geru_renato_coutinho --region us-east-1 --acl public-read-write
 git clone https://github.com/renatoadsumus/geru_app.git artefato_deploy
-zip -r app_geru_eb.zip /artefato_deploy/docker/
+zip -r app_geru_eb.zip /artefato_deploy/aws_eb/
 echo "Enviando artefato_deploy para S3"
 aws s3 sync /artefato_deploy/app_geru_eb.zip s3://bucket_geru_renato_coutinho
