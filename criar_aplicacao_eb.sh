13aaa27b7eae52b58bf2adf80ef15e61a9f4482b
renatoadsumus <renatoadsumus@gmail.com>
2018-05-23 17:20:15 -0300

    Alterando nome do bucket para um formato valido

--- criar_aplicacao_eb.sh
+++ criar_aplicacao_eb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Criando Aplicacao e Deploy Elastic Beanstalk"
 
-aws elasticbeanstalk create-application --application-name app-geru-renato --description "my application"
+aws elasticbeanstalk create-application --application-name app-geru-renato --description "Essa eh uma aplicacao em Pyhton que tera sua execucao em Docker no servico EB"
 
-aws elasticbeanstalk create-application-version --application-name app-geru-renato --version-label v1 --source-bundle S3Bucket=bucket_geru_renato_coutinho,S3Key=app_geru_eb.zip
+aws elasticbeanstalk create-application-version --application-name app-geru-renato --version-label v1 --source-bundle S3Bucket=bucket-geru-renato-coutinho,S3Key=app_geru_eb.zip
 
 echo "Criando Ambiente no Elastic Beanstalk"
 
-aws elasticbeanstalk create-environment --cname-prefix my-cname --application-name app-geru-renato --version-label v1 --environment-name geru_env_docker --solution-stack-name "64bit Amazon Linux 2018.03 v2.10.0 running Docker 17.12.1-ce"
+aws elasticbeanstalk create-environment --cname-prefix app-geru-desafio --application-name app-geru-renato --version-label v1 --environment-name geru_env_docker --solution-stack-name "64bit Amazon Linux 2018.03 v2.10.0 running Docker 17.12.1-ce"
--- criar_bucket_s3.sh
+++ criar_bucket_s3.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Criando Bucket S3"
-aws s3api create-bucket --bucket bucket_geru_renato_coutinho --region us-east-1 --acl public-read-write
+aws s3api create-bucket --bucket bucket-geru-renato-coutinho --region us-east-1 --acl public-read-write
+sleep 2
 git clone https://github.com/renatoadsumus/geru_app.git artefato_deploy
+sleep 2
 zip -r app_geru_eb.zip /artefato_deploy/aws_eb/
 echo "Enviando artefato_deploy para S3"
-aws s3 sync /artefato_deploy/app_geru_eb.zip s3://bucket_geru_renato_coutinho
+sleep 2
+aws s3 sync app_geru_eb.zip s3://bucket-geru-renato-coutinho
