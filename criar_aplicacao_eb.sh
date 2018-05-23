c4b91a260ece4d35eb85cafa6dad5db981c9a5cb
renatoadsumus <renatoadsumus@gmail.com>
2018-05-23 17:33:15 -0300

    Trocando comando SYNC pelo CP para enviar arquivo para S3

--- criar_aplicacao_eb.sh
+++ criar_aplicacao_eb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Criando Aplicacao e Deploy Elastic Beanstalk"
-
 aws elasticbeanstalk create-application --application-name app-geru-renato --description "Essa eh uma aplicacao em Pyhton que tera sua execucao em Docker no servico EB"
 
+sleep 2
 aws elasticbeanstalk create-application-version --application-name app-geru-renato --version-label v1 --source-bundle S3Bucket=bucket-geru-renato-coutinho,S3Key=app_geru_eb.zip
 
 echo "Criando Ambiente no Elastic Beanstalk"
-
+sleep 2
 aws elasticbeanstalk create-environment --cname-prefix app-geru-desafio --application-name app-geru-renato --version-label v1 --environment-name geru_env_docker --solution-stack-name "64bit Amazon Linux 2018.03 v2.10.0 running Docker 17.12.1-ce"
