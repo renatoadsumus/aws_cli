4f938f2ae5d0d5866cb30aff03f5f370e87e9719
renatoadsumus <renatoadsumus@gmail.com>
2018-06-01 17:07:13 -0300

    Incluindo sleep entre Criar S3 e EB

--- deploy_aplicacao_eb.sh
+++ deploy_aplicacao_eb.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+sleep 10
 echo "Deploy nova versao no EB"
 aws elasticbeanstalk create-application-version --application-name app-geru-renato --version-label ${VERSAO} --source-bundle S3Bucket=bucket-geru-renato-coutinho,S3Key=app_geru_eb.zip
 
-sleep 5
+sleep 10
 aws elasticbeanstalk update-environment --environment-name geru-env-docker --version-label ${VERSAO}
+
+sleep 10
