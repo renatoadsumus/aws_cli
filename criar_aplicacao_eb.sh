6476599520d4c3f9eafc2a45c4599487f369b25f
renatoadsumus <renatoadsumus@gmail.com>
2018-06-01 17:24:38 -0300

    Incluindo sleep entre Criar S3 e EB

--- criar_aplicacao_eb.sh
+++ criar_aplicacao_eb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ################## PASSOS PARA CRIAR E DEPLOY NO ELASTIC BEANSTALK ###############
-sleep 2
+sleep 5
 echo "Criando Aplicacao e Deploy Elastic Beanstalk"
 aws elasticbeanstalk create-application --application-name app-geru-renato --description "Essa eh uma aplicacao em Pyhton que tera sua execucao em Docker no servico EB"
 
-sleep 2
+sleep 5
 aws elasticbeanstalk create-application-version --application-name app-geru-renato --version-label  ${VERSAO} --source-bundle S3Bucket=bucket-geru-renato-coutinho,S3Key=app_geru_eb.zip
 
 echo "Criando Ambiente no Elastic Beanstalk"
-sleep 2
+sleep 10
 aws elasticbeanstalk create-environment --cname-prefix app-geru-desafio --application-name app-geru-renato --version-label ${VERSAO} --environment-name geru-env-docker --solution-stack-name "64bit Amazon Linux 2018.03 v2.10.0 running Docker 17.12.1-ce" 
+sleep 10
--- deploy_bucket_s3.sh
+++ deploy_bucket_s3.sh
@@ -6,3 +6,5 @@ zip -r -j app_geru_eb.zip /artefato_deploy/aws_eb/
 echo "Enviando artefato_deploy para S3"
 sleep 5
 aws s3 cp app_geru_eb.zip s3://bucket-geru-renato-coutinho
+
+####http://bucket-geru-renato-coutinho.s3.amazonaws.com/app_geru_eb.zip
