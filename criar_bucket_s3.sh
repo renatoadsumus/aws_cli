6bdb5e9c60a42645186b5d32d094739f8338584c
renatoadsumus <renatoadsumus@gmail.com>
2018-06-03 11:35:32 -0300

    Corrigindo arquivo com variavel de ambiente para criar ambiente EB trocando de Dockerrun.aws.json para environmentvariables.config

--- criar_bucket_s3.sh
+++ criar_bucket_s3.sh
@@ -8,10 +8,10 @@ sleep 2
 git clone https://github.com/renatoadsumus/geru_app.git artefato_deploy
 
 sleep 2
-rm -fr /artefato_deploy/aws_eb/.ebextensions/environmentvariables.config_template
-
 echo "Zipando a pasta artefato_deploy enviar para S3"
-zip -r -j app_geru_eb.zip /artefato_deploy/aws_eb/
+cd /artefato_deploy/aws_eb/
+rm -fr .ebextensions/environmentvariables.config_template
+zip -r app_geru_eb.zip .
 
 sleep 2
 echo "Enviando artefato_deploy para S3"
